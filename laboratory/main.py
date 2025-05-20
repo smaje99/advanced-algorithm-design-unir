c938d71cb0918f9f2dacff221be90d2e8703accd
Sergio Majé <smajefranco@gmail.com>
2025-05-19 19:29:26 -0500

    🎉 Create laboratory of DAA

---
+++ laboratory/main.py
@@ -0,0 +1,10 @@
+"""
+Author: Sergio Andrés Majé Franco
+Date: 2025-05-19
+School: Fundación Universitaria Internacional La Rioja
+Course: Diseño de Algoritmos Avanzados
+Subject: Laboratorio #1: Par más cercano
+
+Description:
+  This is the main entry point for the laboratory exercises.
+"""
